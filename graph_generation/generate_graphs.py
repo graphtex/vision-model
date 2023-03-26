318581bf7844ac97ba6d755a64e2f83b1be933ae
Kevin Wu <Kevin.yuanbo@gmail.com>
2023-03-26 00:01:58 -0500

    files

--- graph_generation/generate_graphs.py
+++ graph_generation/generate_graphs.py
@@ -6,29 +6,26 @@ from random import sample
 import torch
 from torchvision.ops import box_convert
 import math
-import numpy as np
 import os
 from PIL import Image
-from tqdm import tqdm
 import json
 from itertools import cycle
 
 import matplotlib.pyplot as plt
-from matplotlib.font_manager import FontProperties
+
 plt.rcParams['figure.dpi'] = 250
 plt.rcParams['savefig.dpi'] = 250
 
-IMG_LEN=924
+IMG_LEN = 924
 
-
-object_to_code = {'node':36, 
-                  'filled_node':37,
-                  'undirected_BL_TR':38,
-                  'undirected_BR_TL':39,
-                  'directed_BL_TR':40,
-                  'directed_TR_BL':41,
-                  'directed_BR_TL':42,
-                  'directed_TL_BR':43,}
+object_to_code = {'node': 36,
+                  'filled_node': 37,
+                  'undirected_BL_TR': 38,
+                  'undirected_BR_TL': 39,
+                  'directed_BL_TR': 40,
+                  'directed_TR_BL': 41,
+                  'directed_BR_TL': 42,
+                  'directed_TL_BR': 43, }
 for i in range(48, 58):
     object_to_code[chr(i)] = i - 48
 for i in range(97, 123):
@@ -40,35 +37,36 @@ all_labels = list(string.ascii_lowercase) + [chr(num) for num in range(48, 58)]
 def gen_dataset(n, directory):
     os.mkdir(f'./{directory}/train')
     dir_iter = cycle([True, False])
-    n_iter = cycle([i+2 for i in range(14)])
-    color_iter = cycle(['#808080','#696969','#778899','#708090','#2f4f4f','000000'])
-    f_iter = cycle([5,6,7])
+    n_iter = cycle([i + 2 for i in range(14)])
+    color_iter = cycle(['#808080', '#696969', '#778899', '#708090', '#2f4f4f', '000000'])
+    f_iter = cycle([5, 6, 7])
 
     output = {}
     i = 0
     while i < n:
         dir, nodes, f, color = next(dir_iter), next(n_iter), next(f_iter), next(color_iter)
-        for p in np.linspace(0.05, 1.5/nodes,10):
+        for p in np.linspace(0.05, 1.5 / nodes, 10):
             if nodes < 7 and p < 0.15:
                 continue
             # print(dir,nodes,f,p)
 
-            out = gen_graph(nodes,p,f,color,dir,False,i,f"./{directory}/train")
+            out = gen_graph(nodes, p, f, color, dir, False, i, f"./{directory}/train")
             output[i] = out
             i += 1
-    
+
     with open(f'./{directory}/train.json', 'w') as f:
-        json.dump(output, f)        
+        json.dump(output, f)
+
 
 BACKGROUND = Image.open('./paper_background.jpg')
-    
+
 
 # n = number of vertices
 # p = probability of an edge
 # f = font size
 # directed = directed/undirected graph
 # show = whether to display graph
-def gen_graph(n, p, f, c, directed = False, show = False, id=0, directory=''):
+def gen_graph(n, p, f, c, directed=False, show=False, id=0, directory=''):
     # create graph
     G = nx.fast_gnp_random_graph(n, p, directed=directed)
 
@@ -76,71 +74,72 @@ def gen_graph(n, p, f, c, directed = False, show = False, id=0, directory=''):
     labels = dict(zip(nx.spring_layout(G).keys(), sample(all_labels, n)))
     backwards_labels = {labels[key]: key for key in labels}
     nx.relabel_nodes(G, labels, copy=False)
-    
+
     # remove loops
     G.remove_edges_from(list(nx.selfloop_edges(G)))
-    
+
     # adjacency matrix
     adjacency = nx.adjacency_matrix(G).toarray()
 
     # coordinate dictionary
     pos = nx.spring_layout(G, k=2)
-    
+
     # draw settings
     radii = list(np.arange(f + 5, f + 15))
     sample_radii = np.random.choice(radii, n)
     DRAW_OPTIONS = {
         "font_size": f,
         "font_family": 'Bradley Hand',
-        "node_size": sample_radii**2,
+        "node_size": sample_radii ** 2,
         # "arrowsize": 25,
-        "node_color": "#eceeeb", # white
+        "node_color": "#eceeeb",  # white
         "edgecolors": c,
         "font_color": c,
         "edge_color": c,
         "linewidths": 1,
         "width": 1,
     }
-    
+
     # draw graph
     fig = nx.draw_networkx(G, pos, **DRAW_OPTIONS)
     ax = plt.gca()
-    plt.imshow(BACKGROUND,extent=[-2,2,-2,2])
+    plt.imshow(BACKGROUND, extent=[-2, 2, -2, 2])
     ax.set_aspect('equal', adjustable='box')
-    plt.xlim([-2,2])
-    plt.ylim([-2,2])
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
     plt.axis("off")
     plt.savefig(f'{directory}/{id}.png', bbox_inches='tight', pad_inches=0)
     if not show:
         plt.close(fig)
 
     # radii for bounding boxes
-    radius = sample_radii/110
-    letters_radius = f/100
-    
+    radius = sample_radii / 100
+    letters_radius = f / 100
+
     # Generate bounding box points for training data
     def format_box(x1, y1, x2, y2):
         return [min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)]
-
 
     boxes = []
     training_labels = []
     # node boxes
     for k in pos.keys():
         i = backwards_labels[k]
-        boxes.append(format_box(pos[k][0] - radius[i], pos[k][1] - radius[i], pos[k][0] + radius[i], pos[k][1] + radius[i]))
+        boxes.append(
+            format_box(pos[k][0] - radius[i], pos[k][1] - radius[i], pos[k][0] + radius[i], pos[k][1] + radius[i]))
         training_labels.append(object_to_code['node'])
 
     # label boxes
     for k in pos.keys():
-        boxes.append(format_box(pos[k][0] - letters_radius, pos[k][1] - letters_radius, pos[k][0] + letters_radius, pos[k][1] + letters_radius))
+        boxes.append(format_box(pos[k][0] - letters_radius, pos[k][1] - letters_radius, pos[k][0] + letters_radius,
+                                pos[k][1] + letters_radius))
         if k in object_to_code:
             training_labels.append(object_to_code[k])
 
     # edge boxes
     for i in range(len(adjacency)):
         end = i + 1
-        if directed: 
+        if directed:
             end = len(adjacency[i])
         for j in range(0, end):
             letter = labels[i]
@@ -155,10 +154,10 @@ def gen_graph(n, p, f, c, directed = False, show = False, id=0, directory=''):
                 second_right = x2 > x1
                 slope = (y2 - y1) / (x2 - x1)
 
-                x_delta_abs1 = math.sqrt(radius[i]**2 / (1 + slope**2))
+                x_delta_abs1 = math.sqrt(radius[i] ** 2 / (1 + slope ** 2))
                 y_delta_abs1 = abs(slope * x_delta_abs1)
 
-                x_delta_abs2 = math.sqrt(radius[j]**2 / (1 + slope**2))
+                x_delta_abs2 = math.sqrt(radius[j] ** 2 / (1 + slope ** 2))
                 y_delta_abs2 = abs(slope * x_delta_abs2)
 
                 box_x1 = x1 + x_delta_abs1 if second_right else x1 - x_delta_abs1
@@ -166,23 +165,23 @@ def gen_graph(n, p, f, c, directed = False, show = False, id=0, directory=''):
 
                 box_x2 = x2 - x_delta_abs2 if second_right else x2 + x_delta_abs2
                 box_y2 = y2 - y_delta_abs2 if second_above else y2 + y_delta_abs2
-                if box_x2 - box_x1 < 0.1:
-                    add_x = 0.1 - (box_x2 - box_x1)
+                if abs(box_x2 - box_x1) < 0.1:
+                    add_x = abs(0.1 - abs(box_x2 - box_x1))/2
                     box_x1 -= add_x
                     box_x2 += add_x
-                if box_y2 - box_y1 < 0.1:
-                    add_y = 0.1 - (box_y2 - box_y1)
+                if abs(box_y2 - box_y1) < 0.1:
+                    add_y = abs(0.1 - abs(box_y2 - box_y1))/2
                     box_y1 -= add_y
                     box_y2 += add_y
                 boxes.append(format_box(box_x1, box_y1, box_x2, box_y2))
-                
+
                 if not directed:
                     if slope > 0:
                         training_labels.append(object_to_code['undirected_BL_TR'])
                     elif slope <= 0:
                         training_labels.append(object_to_code['undirected_BR_TL'])
                 else:
-                    if second_above and second_right: 
+                    if second_above and second_right:
                         training_labels.append(object_to_code['directed_BL_TR'])
                     elif second_above and not second_right:
                         training_labels.append(object_to_code['directed_BR_TL'])
@@ -193,8 +192,7 @@ def gen_graph(n, p, f, c, directed = False, show = False, id=0, directory=''):
 
     out = {"labels": torch.Tensor([training_labels]), "boxes": torch.Tensor(np.array(boxes))}
 
-    
-    shape = [3,1,1]
+    shape = [3, 1, 1]
     # Transform coordinates
     for i in range(len(out["boxes"])):
         out["boxes"][i][0] = shape[2] / 2 + out["boxes"][i][0] * shape[2] / 4
@@ -202,23 +200,20 @@ def gen_graph(n, p, f, c, directed = False, show = False, id=0, directory=''):
         temp = shape[1] / 2 - out["boxes"][i][1] * shape[1] / 4
         out["boxes"][i][1] = shape[1] / 2 - out["boxes"][i][3] * shape[1] / 4
         out["boxes"][i][3] = temp
-    
-    out['labels'] =[int(i) for i in out['labels'][0]]
-    
 
-    out['boxes'] = out['boxes'].view((-1,4))
+    out['labels'] = [int(i) for i in out['labels'][0]]
+
+    out['boxes'] = out['boxes'].view((-1, 4))
     out['boxes'] = box_convert(out['boxes'], 'xyxy', 'cxcywh')
-
-    # out['boxes'][:3] = torch.maximum(out['boxes'][:3], torch.ones_like(out['boxes'][:3]) * 0.1)
-    # out['boxes'][:4] = torch.maximum(out['boxes'][:4], torch.ones_like(out['boxes'][:3]) * 0.1)
 
     boxes = []
     for i in range(len(out['boxes'])):
         boxes.append([float(i) for i in out['boxes'][i]])
-    
+
     out['boxes'] = boxes
-    
+
     return out
+
 
 def main():
     """n is first arg, directory is second"""
